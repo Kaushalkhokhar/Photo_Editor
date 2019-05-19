fd74571399f8a889bbf3376f566cc0d1e5593142
Kaushal <kaushal2413@gmail.com>
2019-05-19 21:09:59 +0530

    Processed Images Displayed

--- image_processing.py
+++ image_processing.py
@@ -1,5 +1,25 @@
-import matplotlib.pyplot as plt
+import os
+import numpy as np 
+import cv2
+import matplotlib.pyplot as plt 
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+def convolution(filename, plotting):    
+    image_path = os.path.join(APP_ROOT, 'Images/', filename)
+    img = cv2.imread(image_path)
+
+    kernel = np.ones((5,5),np.float32)/25
+    dst = cv2.filter2D(img,-1,kernel)
+
+    if plotting == True:
+        plt.subplot(121),plt.imshow(img),plt.title('Original')
+        plt.xticks([]), plt.yticks([])
+        plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
+        plt.xticks([]), plt.yticks([])
+        plt.show()
+
+    return dst
 
 
 
-    
--- web_app.py
+++ web_app.py
@@ -1,6 +1,7 @@
 import os
+from PIL import Image
 from flask import Flask, render_template, flash, redirect, url_for, request, send_from_directory
-
+from image_processing import convolution
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissecret'
@@ -33,22 +34,49 @@ def upload_image():
             destination = "/".join([target, filename])
             print(destination)
             file.save(destination)
+
+        process = "True"
         
         # return send_from_directory('static', filename, as_attachment=True) # Can be used to download the uploaded images
         return render_template('upload.html', image_name=filename) # In this filename will be the name of image file which is uploaded last in all files
 
 @app.route('/upload/<filename>', methods=['GET', 'POST'])
-def show(filename):        
+def show(filename):
+    print(filename)   
     return send_from_directory('images', filename)
 
-@app.route('/processing', methods=['GET', 'POST'])
-def processing():
+@app.route('/processing/<filename>', methods=['GET', 'POST'])
+def processing(filename):
 
-    return render_template('bootstrap.html')
+    print(filename)
+
+    target = os.path.join(APP_ROOT, 'processed_images/')
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    processed_image = convolution(filename, plotting=False)
+    destination = "/".join([target, filename])
+    print(destination)
+    im = Image.fromarray(processed_image)
+    im.save(destination)
+    
+    # we need to store processed file in our static folder then we can render by giving filename qual to our processed file 
+    return render_template('processing.html', image_name=filename)
+
+@app.route('/processed/<filename>', methods=['GET', 'POST'])
+def processed_image(filename):
+    print(filename)        
+    return send_from_directory('processed_images', filename)
+
+
 
 @app.route('/bootstrap', methods=['GET', 'POST'])
 def bootstrap():        
     return render_template('bootstrap.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
